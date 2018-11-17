bc24eae1c4b4fd0860077e35e84cdf1728a1add2
kuznetsovandrey76 <and.rey.q@yandex.ru>
2018-11-17 23:11:57 +0300

    work with mysql and heroku

--- README.md
+++ README.md
@@ -7,3 +7,24 @@
 - создаем папку `<name>` и переходим в нее 
 `cd /c/OSPanel/domains/<name>`
 - создаем файл `.htaccess` и прописываем кодировку `AddDefaultCharset UTF-8`
+
+`heroku config`
+получим `CLEARDB_DATABASE_URL:mysql://USER:PASSWORD@HOSTNAME/DBASENAME?reconnect=true`
+
+работа с бд
+`https://www.heidisql.com/download.php?download=installer`
+
+Доступ к базе в php
+```
+$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"],1);
+
+    mysqli_connect($server, $username, $password);
+    mysqli_select_db($db);
+```
+MySQL Installer
+`https://dev.mysql.com/downloads/installer/`
--- index.php
+++ index.php
@@ -1,7 +1,35 @@
-<p>html - текст</p>
-
-<?php 
+<?php
+if(isset($_POST["done"])) {
+	if($_POST["firstname"] === "") {
+		echo "Введите имя. <a href='/'>Исправить</a>"; 
+	} else {
+		header("Location:index.php");
+	}
+} 
 // php - personal home page 
-$string = 'php - текст';
-echo $string . "<br>new"; 
+// echo PHP_VERSION; 
+echo print_r($_POST);	
+
+$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"],1);
+
+    mysqli_connect($server, $username, $password);
+    mysqli_select_db($db);
+
+
 ?>
+	
+
+<body>
+	
+<form name="data" action="index.php" method="POST">
+	<input type="text" name="firstname" placeholder="First name">
+	<input type="text" name="lastname" placeholder="Last name">
+	<input type="submit" name="done" value="Send">	
+</form>
+
+</body>
